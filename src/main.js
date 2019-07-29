e5ed3c294402018a6fd999bc296c8cbfb43da30b
creativo <creativo@comunicacionvisualcanarias.com>
2019-07-29 14:55:03 +0100

    WIP 290719

--- src/main.js
+++ src/main.js
@@ -10,26 +10,17 @@ var configWin = null
 =            Preferencias            =
 =============================================*/
 
-const PREFS_FILE = `${app.getPath('userData')}/appConf.json`
+  const PREFS_FILE = `${app.getPath('userData')}/appConf.json`
 
-// Defaults
-const DEFAULTS = 
-{ 
-  ip:'127.0.0.1', 
-  colas:['Mostrador 1','Mostrador 2','Mostrador 3'],
-  focusOnShortcut:false 
-}
-global.appConf = DEFAULTS
+  // Defaults
+  const DEFAULTS = 
+  { 
+    ip:'127.0.0.1', 
+    colas:['Mostrador 1','Mostrador 2','Mostrador 3'],
+    focusOnShortcut:false 
+  }
+  global.appConf = DEFAULTS
 
-function reloadApp() {
-  app.relaunch()
-  app.exit()
-}
-
-function turno(accion) {
-  appWin.webContents.send('turnomatic', accion)
-  if (appConf.focusOnShortcut) { appWin.focus() }
-}
 
 /*=====  End of Preferencias  ======*/
 
@@ -40,33 +31,33 @@ function turno(accion) {
 =            Menu            =
 =============================================*/
 
-const menu = [
-  {
-      role: 'appMenu',
-      label: 'Archivo',
+  const menu = [
+    {
+        role: 'appMenu',
+        label: 'Archivo',
+        submenu: [
+            {label:'Recargar',  click() { reloadApp() } },
+            {role: 'quit', label:'Salir'},
+        ]
+    },{
+        label: 'Editar',
+        submenu: [
+            {label:'Ajustes',   click() {
+              if (configWin == null)  { config() } 
+              else                    { configWin.focus() } 
+            }},
+            {type: 'separator'},
+            {label:'Restaurar parámetros',     click() { restoreDialog() } }
+        ]
+    }
+    ,{
+      role: 'help',
+      label: 'Ayuda',
       submenu: [
-          {label:'Recargar',  click() { reloadApp() } },
-          {role: 'quit', label:'Salir'},
-      ]
-  },{
-      label: 'Editar',
-      submenu: [
-          {label:'Ajustes',   click() {
-            if (configWin == null)  { config() } 
-            else                    { configWin.focus() } 
-          }},
-          {type: 'separator'},
-          {label:'Restaurar parámetros',     click() { restoreDialog() } }
+          {role: 'about', label:'Información',     click() { about() } }
       ]
   }
-  ,{
-    role: 'help',
-    label: 'Ayuda',
-    submenu: [
-        {role: 'about', label:'Información',     click() { about() } }
-    ]
-}
-]
+  ]
 
 /*=====  End of Menu  ======*/
 
@@ -77,23 +68,33 @@ const menu = [
 =            Funciones            =
 =============================================*/
 
-function savePrefs(prefs, reload=true) {
-  global.appConf = prefs
-  fs.writeFileSync(PREFS_FILE, JSON.stringify(global.appConf), 'utf8')
-}
+  function turno(accion) {
+    appWin.webContents.send('turnomatic', accion)
+    if (appConf.focusOnShortcut) { appWin.focus() }
+  }
 
-function restore() {
-  savePrefs(DEFAULTS)
-}
+  function reloadApp() {
+    app.relaunch()
+    app.exit()
+  }
 
-function restoreDialog() {
-  const options  = {
-    type: 'warning',
-    buttons: ['Cancelar','Aceptar'],
-    message: '¿Restaurar los valores por defecto de la configuración de la aplicación?'
-   }
-  dialog.showMessageBox(options, (resp) => { if (resp) { restore(); reloadApp() } }) // Ha pulsado aceptar
-}
+  function savePrefs(prefs, reload=true) {
+    global.appConf = prefs
+    fs.writeFileSync(PREFS_FILE, JSON.stringify(global.appConf), 'utf8')
+  }
+
+  function restore() {
+    savePrefs(DEFAULTS)
+  }
+
+  function restoreDialog() {
+    const options  = {
+      type: 'warning',
+      buttons: ['Cancelar','Aceptar'],
+      message: '¿Restaurar los valores por defecto de la configuración de la aplicación?'
+    }
+    dialog.showMessageBox(options, (resp) => { if (resp) { restore(); reloadApp() } }) // Ha pulsado aceptar
+  }
 
 /*=====  End of Funciones  ======*/
 
@@ -104,61 +105,60 @@ function restoreDialog() {
 =            Ventanas            =
 =============================================*/
 
-function initApp() {
-  appWin = new BrowserWindow({width: 600,height: 300, show:false, webPreferences: { nodeIntegration: true}})
-  
-  appWin.loadURL(`file://${__dirname}/index.html`)
-  appWin.setMenu( Menu.buildFromTemplate(menu) )
-  appWin.setResizable( false )
-  
-  globalShortcut.register('CommandOrControl+1', () => { turno('sube') })
-  globalShortcut.register('CommandOrControl+2', () => { turno('baja') })
-  globalShortcut.register('CommandOrControl+3', () => { turno('reset') })
-  
-  appWin.show()
-  appWin.on('closed', () => { app.quit() })
-  
-  //appWin.webContents.openDevTools()
+  function initApp() {
+    appWin = new BrowserWindow({width: 600,height: 300, show:false, webPreferences: { nodeIntegration: true}})
+    
+    appWin.loadURL(`file://${__dirname}/index.html`)
+    appWin.setMenu( Menu.buildFromTemplate(menu) )
+    appWin.setResizable( false )
+    
+    globalShortcut.register('CommandOrControl+1', () => { turno('sube') })
+    globalShortcut.register('CommandOrControl+2', () => { turno('baja') })
+    globalShortcut.register('CommandOrControl+3', () => { turno('reset') })
+    
+    appWin.show()
+    appWin.on('closed', () => { app.quit() })
+    
+    //appWin.webContents.openDevTools()
 
-  if (fs.existsSync(PREFS_FILE)) {
-    try {
-      global.appConf = JSON.parse(fs.readFileSync(PREFS_FILE, 'utf8'))
-    } catch (error) {
-      appWin.hide()
-      const options  = {
-        type: 'error',
-        buttons: ['Aceptar'],
-        message: 'El archivo de configuración está dañado. Se restaurarán los parámetros por defecto de la aplicación.'
-       }
-  
-      dialog.showMessageBox(appWin, options, () => { restore(); app.relaunch(); app.exit() }) // Ha pulsado aceptar
+    if (fs.existsSync(PREFS_FILE)) {
+      try {
+        global.appConf = JSON.parse(fs.readFileSync(PREFS_FILE, 'utf8'))
+      } catch (error) {
+        appWin.hide()
+        const options  = {
+          type: 'error',
+          buttons: ['Aceptar'],
+          message: 'El archivo de configuración está dañado. Se restaurarán los parámetros por defecto de la aplicación.'
+        }
+    
+        dialog.showMessageBox(appWin, options, () => { restore(); app.relaunch(); app.exit() }) // Ha pulsado aceptar
+      }
     }
   }
-}
 
+  function config() {
+    configWin = new BrowserWindow({width: 400,height: 600, show:false, webPreferences: { nodeIntegration: true, parent: appWin }})
+    configWin.loadURL(`file://${__dirname}/config.html`)
+    configWin.setMenu( null )
+    configWin.setResizable( false )
+    configWin.show()
+    
+    configWin.on('closed', () => { configWin = null })
 
-function config() {
-  configWin = new BrowserWindow({width: 400,height: 600, show:false, webPreferences: { nodeIntegration: true, parent: appWin }})
-  configWin.loadURL(`file://${__dirname}/config.html`)
-  configWin.setMenu( null )
-  configWin.setResizable( false )
-  configWin.show()
-  
-  configWin.on('closed', () => { configWin = null })
+    //configWin.webContents.openDevTools()
+  }
 
-  //configWin.webContents.openDevTools()
-}
+  function about() {
+    const options  = {
+      type: 'info',
+      buttons: ['Aceptar'],
+      message: 'Farmavisión - Control Turnomatic para PC\nComunicacion Visual Canarias 2019\nContacto: 928 67 29 81'
+     }
+    dialog.showMessageBox(appWin, options)
+  }
 
 /*=====  End of Ventanas  ======*/
-
-function about() {
-  const options  = {
-    type: 'info',
-    buttons: ['Aceptar'],
-    message: 'Farmavisión - Control Turnomatic para PC\nComunicacion Visual Canarias 2019\nContacto: 928 67 29 81'
-   }
-  dialog.showMessageBox(appWin, options)
-}
 
 
 
